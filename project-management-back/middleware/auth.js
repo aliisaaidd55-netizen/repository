9d8e872f35beb945e9c6b21092a016910312f033
aliisaaidd55-netizen <aliisaaidd55@gmail.com>
2025-12-03 23:00:11 +0200

    Create auth.js

---
+++ project-management-back/middleware/auth.js
@@ -0,0 +1,22 @@
+// backend/middleware/auth.js
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Not authorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token invalid" });
+  }
+};
+
+export const managerOnly = (req, res, next) => {
+  if (req.user.role !== "manager") {
+    return res.status(403).json({ message: "Manager access only" });
+  }
+  next();
+};
