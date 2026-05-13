595f202b76001943992caa7ec3110b079f240886
aliisaaidd55-netizen <aliisaaidd55@gmail.com>
2026-05-13 04:32:38 +0300

    Obscure sensitive MongoDB connection details

--- project-management-back/config/db.js
+++ project-management-back/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://final_project:Kv4mCMDkCuu3CZLG@cluster0.gwquyzz.mongodb.net/All-data?appName=Cluster0');
+        const conn = await mongoose.connect('mongodb+srv://****_project:****************.mongodb.net/All-data?appName=Clust***');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log("MongoDB Error: ", error.message);
